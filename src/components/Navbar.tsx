24e3f6835dbca8619dabed8a25bb435b550c6027
Brendan Ooi <brendanooi16@gmail.com>
2024-03-31 00:59:49 +0800

    more navbar progress

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,11 @@
 import "../styles/Navbar.css";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { BiMessageAltDetail } from "react-icons/bi";
+import { IoPeopleOutline } from "react-icons/io5";
+import { HiOutlineClipboardList } from "react-icons/hi";
+import { MdSchedule } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+import { BiHelpCircle } from "react-icons/bi";
 import hiltilogo from "../assets/Hilti-Logo.jpg";
 function Navbar() {
   return (
@@ -8,17 +15,42 @@ function Navbar() {
           <img src={hiltilogo} className="brand-logo" />
         </div>
         <ul className="nav-menu">
-          <li>Dashboard</li>
-          <li>Messages</li>
-          <li>Company Profile</li>
-          <li>All Applicants</li>
-          <li>Job Listing</li>
-          <li>My Schedule</li>
+          <li>
+            <BiHomeAlt2 className="navbar-icon" />
+            Dashboard
+          </li>
+          <li>
+            <BiMessageAltDetail className="navbar-icon" />
+            Messages
+          </li>
+          <li>
+            <IoPeopleOutline className="navbar-icon" />
+            All Applicants
+          </li>
+          <li>
+            <HiOutlineClipboardList className="navbar-icon" />
+            Job Listing
+          </li>
+          <li>
+            <MdSchedule className="navbar-icon" />
+            My Schedule
+          </li>
         </ul>
-        <hr />
+        <hr className="navbar-line" />
+        <div className="settings">
+          <p className="navbar-settings-title">SETTINGS</p>
+          <ul className="nav-menu">
+            <li>
+              <FiSettings className="navbar-icon" />
+              Settings
+            </li>
+            <li>
+              <BiHelpCircle className="navbar-icon" />
+              Help Center
+            </li>
+          </ul>
+        </div>
       </nav>
-
-      <div className="settings"></div>
     </>
   );
 }
