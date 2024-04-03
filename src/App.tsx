09d7101f30fc1b6c375032c6e1f46eda9be98d65
haarishnair <157096235+haarishnair@users.noreply.github.com>
2024-04-03 12:21:42 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./styles/App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard.tsx";
-import Navbar from "./components/Navbar/Navbar.tsx";
 import Applicant from "./components/Applicant.tsx";
-import CVUpload from "./components/CVUpload.tsx";
-import ApplicantsRanking from "./components/ApplicantsRanking/ApplicantsRanking.tsx";
+import CV from "./components/CV-upload.tsx";
+import Login from "./components/LoginPage.tsx";
+import Register from "./components/RegisterPage.tsx";
+
+
+
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route index element = { <Login></Login> }></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/applicant" element={<Applicant />} />
-          <Route path="/cv-upload" element={<CVUpload />} />
-          <Route path="/all-applicants" element={<ApplicantsRanking />} />
+          <Route path="/CV" element={<CV />} />
         </Routes>
       </Router>
     </>
